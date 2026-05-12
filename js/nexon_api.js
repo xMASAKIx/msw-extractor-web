9aba7abccecc367b2c4b6337b94aba861c0a1a71
xMASAKIx <110505678+xMASAKIx@users.noreply.github.com>
2026-05-12 09:31:39 +0800

    Update nexon_api.js

--- js/nexon_api.js
+++ js/nexon_api.js
@@ -70,6 +70,8 @@ const NexonAPI = {
                     nickname: profile?.profileName || d.nickname || d.profileName || "未知",
                     profileCode: profile?.profileCode || d.profileCode || "", 
                     itemId: d.itemId || d.id || input
+                    name: d.avatarName || d.name || "",      // 確保這裡有對應到 name
+                    avatarType: d.avatarType || ""
                 };
             }
         } catch (e) { 
